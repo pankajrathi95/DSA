4ee262361f0ef83e50704336574166331ddae56c
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-04-05 20:32:21 +0530

    Day - 5 Leetcode 30days Challenge

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,6 +7,8 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BuyAndSellStock buyAndSellStock = new BuyAndSellStock();
+            buyAndSellStock.MaxProfit(new int[] { 2, 4, 1 });
         }
     }
 }
