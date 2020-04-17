86375a6f7c8eac31a4ebaeb2de63875d3ae867ff
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-04-17 14:14:25 +0530

    one more

--- DataStructures/src/LeetCode/30DayLeetCodeCallenge/ValidParenthesisString.cs
+++ DataStructures/src/LeetCode/30DayLeetCodeCallenge/ValidParenthesisString.cs
@@ -1,39 +1,71 @@
+/*
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+An empty string is also valid.
+Example 1:
+Input: "()"
+Output: True
+Example 2:
+Input: "(*)"
+Output: True
+Example 3:
+Input: "(*))"
+Output: True
+
+*/
+
+
 using System.Collections.Generic;
 
 public class ValidParenthesisString
 {
     public bool CheckValidString(string s)
     {
-        Stack<char> myStack = new Stack<char>();
-        int starCount = 0;
+        int balance = 0;
         foreach (char ch in s)
         {
-            if (ch == '(')
+            if (ch == ')')
             {
-                myStack.Push(ch);
-            }
-            else if (ch == ')')
-            {
-                if (myStack.Count == 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    myStack.Pop();
-                }
-
+                balance--;
             }
             else
             {
-                starCount++;
+                balance++;
+            }
+
+            if (balance < 0)
+            {
+                return false;
             }
         }
 
-        if (myStack.Count <= starCount || (myStack.Count == 0))
+        if (balance == 0)
         {
             return true;
         }
-        return false;
+
+        balance = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '(')
+            {
+                balance--;
+            }
+            else
+            {
+                balance++;
+            }
+
+            if (balance < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
