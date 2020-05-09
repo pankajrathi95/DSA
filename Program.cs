b8a39bb2ee8b830e2b46591a21a19bf43b363755
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-05-09 19:08:34 +0530

    Tries

--- Program.cs
+++ Program.cs
@@ -10,31 +10,14 @@ namespace DataStructures
         {
             Console.WriteLine("Hello World!");
 
-            // BinaryTree tree = new BinaryTree();
-            // tree.Insert(7);
-            // tree.Insert(4);
-            // tree.Insert(9);
-            // tree.Insert(1);
-            // tree.Insert(6);
-            // tree.Insert(8);
-            // tree.Insert(10);
-
-            // Console.WriteLine(tree.GetAncestors(81));
-
-            // Heapify heapify = new Heapify();
-            // int[] values = heapify.HeapifyIt(new int[] { 5, 3, 8, 4, 1, 2 });
-
-            // Trie trie = new Trie();
-            // trie.Insert("cat");
-            // trie.Insert("calm");
-            // trie.Remove("cat");
-            // //Console.WriteLine(trie.Contains("ca"));
-            // Console.WriteLine(trie.Contains("cat"));
-            // //Console.WriteLine(trie.Contains("cal"));
-            // Console.WriteLine(trie.Contains("calm"));
-            // // trie.Traverse();
-            PerfectSquare perfect = new PerfectSquare();
-            perfect.IsPerfectSquare(14);
+            Trie trie = new Trie();
+            trie.Insert("car");
+            trie.Insert("card");
+            trie.Insert("care");
+            trie.Insert("careful");
+            bool value = trie.ContainsRecursive("cars");
+            int count = trie.CountWords();
+            string tmep = Trie.LongestCommonPrefix(new string[] { "car", "card", "cardkdd" });
         }
     }
 }
--- src/CodeWithMosh/DataStructures - Part-2/Trie.cs
+++ src/CodeWithMosh/DataStructures - Part-2/Trie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 public class Trie
 {
@@ -25,6 +26,10 @@ public class Trie
 
         public Node GetChild(char ch)
         {
+            if (!children.ContainsKey(ch))
+            {
+                return null;
+            }
             return children[ch];
         }
 
@@ -117,5 +122,135 @@ public class Trie
         {
             child.RemoveChild(ch);
         }
+    }
+
+    public List<string> AutoComplete(string word)
+    {
+        List<string> words = new List<string>();
+
+        var current = root;
+        foreach (char ch in word)
+        {
+            var child = current.GetChild(ch);
+
+            if (child == null)
+            {
+                return null;
+            }
+
+            current = child;
+        }
+
+        AutoComplete(current, word, words);
+        return words;
+    }
+
+    private void AutoComplete(Node node, string word, List<string> words)
+    {
+        if (node.isEndOfWord)
+        {
+            words.Add(word);
+        }
+
+        foreach (var child in node.GetChildren())
+        {
+            AutoComplete(child.Value, word + child.Value.value, words);
+        }
+    }
+
+    public bool ContainsRecursive(string word)
+    {
+        return ContainsRecursive(root, 0, word);
+    }
+
+    private bool ContainsRecursive(Node node, int i, string word)
+    {
+        if (i == word.Length)
+            return root.isEndOfWord;
+
+        if (root == null)
+            return false;
+
+        var ch = word[i];
+        var child = root.GetChild(ch);
+        if (child == null)
+            return false;
+
+        return ContainsRecursive(child, i + 1, word);
+    }
+
+    public int CountWords()
+    {
+        int count = 0;
+        count = CountWords(root);
+        return count;
+    }
+
+    private int CountWords(Node node)
+    {
+        int count = 0;
+        if (node == null)
+        {
+            return count;
+        }
+
+        if (node.isEndOfWord)
+        {
+            count = count + 1;
+        }
+
+        foreach (var child in node.GetChildren())
+        {
+            count += CountWords(child.Value);
+        }
+
+        return count;
+    }
+
+    public static string LongestCommonPrefix(string[] words)
+    {
+        Trie trie = new Trie();
+        foreach (var word in words)
+        {
+            trie.Insert(word);
+        }
+
+        var maxChars = GetShortestWord(words).Length;
+        var prefix = new StringBuilder();
+        var current = trie.root;
+        while (prefix.Length < maxChars)
+        {
+            var children = current.GetChildren();
+
+            if (children.Count != 1)
+            {
+                break;
+            }
+            var temp = new Node[maxChars];
+            children.Values.CopyTo(temp, 0);
+            current = temp[0];
+            prefix.Append(current.value);
+        }
+
+        return prefix.ToString();
+    }
+
+    private static string GetShortestWord(string[] words)
+    {
+        if (words == null || words.Length == 0)
+        {
+            return string.Empty;
+        }
+
+        var shortest = words[0];
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length < shortest.Length)
+            {
+                shortest = words[i];
+            }
+        }
+
+        return shortest;
     }
 }
