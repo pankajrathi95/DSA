f7fc18843cc33522c52d3de9bb36d6b75b2a2246
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-05-11 22:58:34 +0530

    Day 11

--- Program.cs
+++ Program.cs
@@ -10,14 +10,23 @@ namespace DataStructures
         {
             Console.WriteLine("Hello World!");
 
-            Trie trie = new Trie();
-            trie.Insert("car");
-            trie.Insert("card");
-            trie.Insert("care");
-            trie.Insert("careful");
-            bool value = trie.ContainsRecursive("cars");
-            int count = trie.CountWords();
-            string tmep = Trie.LongestCommonPrefix(new string[] { "car", "card", "cardkdd" });
+            // Trie trie = new Trie();
+            // trie.Insert("car");
+            // trie.Insert("card");
+            // trie.Insert("care");
+            // trie.Insert("careful");
+            // bool value = trie.ContainsRecursive("cars");
+            // int count = trie.CountWords();
+            // string tmep = Trie.LongestCommonPrefix(new string[] { "car", "card", "cardkdd" });
+
+            int[][] image = new int[2][];
+
+            image[0] = new int[] { 0, 0, 0 };
+            image[1] = new int[] { 0, 1, 1 };
+            //image[2] = new int[] { 1, 0, 1 };
+
+            FloodFill floodFill = new FloodFill();
+            image = floodFill.FloodFillIt(image, 1, 1, 1);
         }
     }
 }
