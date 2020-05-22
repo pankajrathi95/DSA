6c815f4c218a8ec7bc9e69146365fb5814ef7b08
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-05-22 18:10:22 +0530

    Implement Str str

--- Program.cs
+++ Program.cs
@@ -8,13 +8,8 @@ namespace DataStructures
     {
         public static void Main(string[] args)
         {
-            int[][] matrix = new int[3][];
-            matrix[0] = new int[] { 0, 1, 1, 1 };
-            matrix[1] = new int[] { 1, 1, 1, 1 };
-            matrix[2] = new int[] { 0, 1, 1, 1 };
-
-            CountSquareSubmatricesWithAllOnes allOnes = new CountSquareSubmatricesWithAllOnes();
-            allOnes.CountSquares(matrix);
+            ImplementStrStr implement = new ImplementStrStr();
+            implement.StrStr("mississippi", "issip");
         }
     }
 }
---
+++ src/LeetCode/Easy/ImplementStrStr.cs
@@ -0,0 +1,55 @@
+/*
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+*/
+
+public class ImplementStrStr
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (needle == "")
+        {
+            return 0;
+        }
+
+        int start = 0;
+
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            start = 0;
+            int j = i;
+            while (start < needle.Length && j < haystack.Length)
+            {
+                if (needle[start] != haystack[j])
+                {
+                    break;
+                }
+
+                start++;
+                j++;
+            }
+
+            if (start == needle.Length)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
