d8ac8db080c15d8ccf90ce4e0dd01f9fd2671b77
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-05-28 11:45:49 +0530

    Day 27

--- src/LeetCode/30DayLeetCodeChallenge-May2020/PossibleBipartition.cs
+++ src/LeetCode/30DayLeetCodeChallenge-May2020/PossibleBipartition.cs
@@ -35,10 +35,61 @@ There does not exist i != j for which dislikes[i] == dislikes[j].
 
 */
 
+using System.Collections.Generic;
+
 public class PossibleBipartition
 {
-    public bool PossibleBipartition(int N, int[][] dislikes)
+    Dictionary<int, HashSet<int>> map;
+    Dictionary<int, int> groups;
+    public bool FindPossibleBipartition(int N, int[][] dislikes)
     {
 
+        map = new Dictionary<int, HashSet<int>>();
+        groups = new Dictionary<int, int>();
+        int m = dislikes.Length;
+        for (int i = 1; i <= N; i++)
+        {
+            map.Add(i, new HashSet<int>());
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int from = dislikes[i][0];
+            int to = dislikes[i][1];
+
+            map[from].Add(to);
+            map[to].Add(from); //essential.
+        }
+        for (int i = 1; i <= N; i++)
+        {
+            if (!groups.ContainsKey(i))
+            {
+                if (dfs(i, 1))
+                {
+                    //means i and its dislikes belong to different groups
+                    //we are good
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private bool dfs(int i, int groupId)
+    {
+        if (groups.ContainsKey(i))
+        {
+            return groups[i] == groupId;
+        }
+        groups.Add(i, groupId);
+
+
+        foreach (int dislike in map[i])
+        {
+            if (!dfs(dislike, groupId == 1 ? 2 : 1))
+                return false;
+        }
+        return true;
     }
 }
