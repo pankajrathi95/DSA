65b09db59ec5354e12edf0f1679044b05cb1bf0f
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-06-15 18:30:55 +0530

    Day 15

---
+++ src/LeetCode/30DayLeetCodeChallenge-June2020/SearchInABinaryTree.cs
@@ -0,0 +1,66 @@
+/*
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+*/
+
+public class SearchInABinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    TreeNode currentRoot = null;
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        SearchBT(root, val);
+        return currentRoot;
+    }
+
+    public void SearchBT(TreeNode root, int val)
+    {
+        if (root == null && currentRoot == null)
+        {
+            currentRoot = null;
+            return;
+        }
+        else if (root == null && currentRoot != null)
+        {
+            return;
+        }
+
+        if (root != null && root.val == val)
+        {
+            currentRoot = root;
+            return;
+        }
+
+        SearchBT(root.left, val);
+        SearchBT(root.right, val);
+    }
+}
