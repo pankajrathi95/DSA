ea384dcf1c6fd044bbe082f16dff82a0d22fd8c1
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-06-22 06:56:59 +0530

    Weighted Graphs

--- Program.cs
+++ Program.cs
@@ -8,28 +8,38 @@ namespace DataStructures
         {
             Console.WriteLine("Hello World!");
 
-            Graph graph = new Graph();
+            // Graph graph = new Graph();
 
-            graph.AddNode("A");
-            graph.AddNode("B");
-            graph.AddNode("C");
-            graph.AddNode("D");
-            graph.AddNode("E");
+            // graph.AddNode("A");
+            // graph.AddNode("B");
+            // graph.AddNode("C");
+            // graph.AddNode("D");
+            // graph.AddNode("E");
 
-            graph.AddEdge("A", "B");
-            graph.AddEdge("A", "E");
+            // graph.AddEdge("A", "B");
+            // graph.AddEdge("A", "E");
 
-            graph.AddEdge("B", "E");
+            // graph.AddEdge("B", "E");
 
-            graph.AddEdge("C", "A");
-            graph.AddEdge("C", "B");
-            graph.AddEdge("C", "D");
+            // graph.AddEdge("C", "A");
+            // graph.AddEdge("C", "B");
+            // graph.AddEdge("C", "D");
 
-            graph.AddEdge("D", "E");
+            // graph.AddEdge("D", "E");
 
 
 
-            Console.WriteLine(graph.HasCycle());
+            // Console.WriteLine(graph.HasCycle());
+
+            WeightedGraph weightedGraph = new WeightedGraph();
+            weightedGraph.AddNode("A");
+            weightedGraph.AddNode("B");
+            weightedGraph.AddNode("C");
+
+            weightedGraph.AddEdge("A", "B", 10);
+            weightedGraph.AddEdge("A", "C", 20);
+
+            weightedGraph.Print();
         }
     }
 }
---
+++ src/CodeWithMosh/DataStructures - Part-2/WeightedGraph.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+
+public class WeightedGraph
+{
+    internal class Node
+    {
+        private string label;
+
+        private List<Edge> edges = new List<Edge>();
+
+        public Node(string label)
+        {
+            this.label = label;
+        }
+
+        public override string ToString()
+        {
+            return label;
+        }
+
+        public void AddEdge(Node to, int weight)
+        {
+            edges.Add(new Edge(this, to, weight));
+        }
+
+        public List<Edge> GetEdges()
+        {
+            return edges;
+        }
+    }
+
+    internal class Edge
+    {
+        internal Node from;
+        internal Node to;
+        internal int weight;
+
+        public Edge(Node from, Node to, int weight)
+        {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return from + " -> " + to;
+        }
+    }
+
+    internal Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+
+    //internal Dictionary<Node, List<Edge>> edges = new Dictionary<Node, List<Edge>>();
+
+    public void AddNode(string label)
+    {
+        var node = new Node(label);
+
+        if (!nodes.ContainsKey(label))
+        {
+            nodes.Add(label, node);
+        }
+
+        /*if (!edges.ContainsKey(node))
+        {
+            edges.Add(node, new List<Edge>());
+        }*/
+    }
+
+    public void AddEdge(string from, string to, int weight)
+    {
+        var fromNode = nodes.GetValueOrDefault(from);
+        if (fromNode == null)
+        {
+            throw new InvalidProgramException();
+        }
+
+        var toNode = nodes.GetValueOrDefault(to);
+        if (toNode == null)
+        {
+            throw new InvalidProgramException();
+        }
+
+        fromNode.AddEdge(toNode, weight);
+        toNode.AddEdge(fromNode, weight);
+        //edges.GetValueOrDefault(fromNode).Add(new Edge(fromNode, toNode, weight));
+        //edges.GetValueOrDefault(toNode).Add(new Edge(toNode, fromNode, weight));
+    }
+
+    public void Print()
+    {
+        foreach (var source in nodes.Values)
+        {
+            var targets = source.GetEdges();
+            if (targets.Count != 0)
+            {
+                Console.WriteLine(source.ToString() + " is connected to " + NodePrint(targets));
+            }
+        }
+    }
+
+    private string NodePrint(List<Edge> values)
+    {
+        string value = "[ ";
+        foreach (var node in values)
+        {
+            value += node.ToString() + ", ";
+        }
+
+        value += " ]";
+        return value;
+    }
+}
