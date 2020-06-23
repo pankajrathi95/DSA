8f75a1ecb038be717d5128c80dae99be15e4c436
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-06-23 22:15:04 +0530

    Day 23

---
+++ src/LeetCode/30DayLeetCodeChallenge-June2020/CountCompleteTreeNodes.cs
@@ -0,0 +1,49 @@
+/*
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input: 
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+*/
+
+public class CountCompleteTreeNodes
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int CountNodes(TreeNode root)
+    {
+        return CountNode(root);
+    }
+
+    private int CountNode(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        return 1 + CountNode(root.left) + CountNode(root.right);
+    }
+}
