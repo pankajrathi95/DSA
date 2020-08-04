d2817c9e7dd59d3f9a91c4be449a81fab8e37196
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-04 21:43:41 +0530

    Add Two Numbers - LeetCode - Medium

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace DataStructures
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddTwoNumbers.ListNode l1 = new AddTwoNumbers.ListNode(2, new AddTwoNumbers.ListNode(4, new AddTwoNumbers.ListNode(3, new AddTwoNumbers.ListNode(6, new AddTwoNumbers.ListNode(9)))));
+            AddTwoNumbers.ListNode l2 = new AddTwoNumbers.ListNode(5, new AddTwoNumbers.ListNode(6, new AddTwoNumbers.ListNode(4)));
+            AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+            addTwoNumbers.AddTheTwoNumbers(l1, l2);
         }
     }
 }
---
+++ src/LeetCode/Medium/AddTwoNumbers.cs
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/add-two-numbers/
+*/
+
+public class AddTwoNumbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode AddTheTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int carry = 0;
+        var head = new ListNode();
+        var current = head;
+        while (l1 != null && l2 != null)
+        {
+            current.next = new ListNode((l1.val + l2.val) % 10 + carry);
+            carry = (l1.val + l2.val) / 10;
+            if (l1.next == null && l2.next != null)
+            {
+                current = current.next;
+                l2.next.val += carry;
+                current.next = l2.next;
+                break;
+            }
+            else if (l1.next != null && l2.next == null)
+            {
+                current = current.next;
+                l1.next.val += carry;
+                current.next = l1.next;
+                break;
+            }
+
+            current = current.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return head.next;
+    }
+}
