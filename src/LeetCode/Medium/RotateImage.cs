d637c5cbbc14c69a53b2515d070522efe0e388b1
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-15 23:02:42 +0530

    Rotate Image - LeetCode

---
+++ src/LeetCode/Medium/RotateImage.cs
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/rotate-image/
+*/
+public class RotateImage
+{
+    public void Rotate(int[][] matrix)
+    {
+        int end = matrix.Length - 1, start = 0;
+        while (start < end)
+        {
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int temp = matrix[start][i];
+                matrix[start][i] = matrix[end][i];
+                matrix[end][i] = temp;
+            }
+
+            start++;
+            end--;
+        }
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
