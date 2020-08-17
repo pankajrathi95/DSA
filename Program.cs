f0bc9fe65141cd950a9055bccbedc6784975695a
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-17 15:23:36 +0530

    Day 17 - Leetcode

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ namespace DataStructures
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
-            int[] prices = new int[] { 1, 2, 4, 2, 5, 7, 2, 4, 9, 0 };
-            BestTimetoBuyandSellStockIII bestTimetoBuyandSell = new BestTimetoBuyandSellStockIII();
-            bestTimetoBuyandSell.MaxProfit(prices);
+            // int[] prices = new int[] { 1, 2, 4, 2, 5, 7, 2, 4, 9, 0 };
+            // BestTimetoBuyandSellStockIII bestTimetoBuyandSell = new BestTimetoBuyandSellStockIII();
+            // bestTimetoBuyandSell.MaxProfit(prices);
+
+            DistributeCandiestoPeople candiestoPeople = new DistributeCandiestoPeople();
+            candiestoPeople.DistributeCandies(80, 4);
         }
     }
 }
