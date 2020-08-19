b0aba038864e9e122c16232705bb199da4627017
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-19 14:41:27 +0530

    Day 19

---
+++ src/LeetCode/30DayLeetCodeChallenge-August2020/GoatLatin.cs
@@ -0,0 +1,68 @@
+/*
+A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+The rules of Goat Latin are as follows:
+
+If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+For example, the word 'apple' becomes 'applema'.
+ 
+If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+ 
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+Return the final sentence representing the conversion from S to Goat Latin. 
+
+ 
+
+Example 1:
+
+Input: "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+Example 2:
+
+Input: "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ 
+
+Notes:
+
+S contains only uppercase, lowercase and spaces. Exactly one space between each word.
+1 <= S.length <= 150.
+*/
+
+using System;
+using System.Text;
+
+public class GoatLatin
+{
+    public string ToGoatLatin(string S)
+    {
+        if (S.Length == 0)
+        {
+            return "";
+        }
+        string[] words = S.Split(' ');
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        foreach (var word in words)
+        {
+            if (word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u' ||
+                word[0] == 'A' || word[0] == 'E' || word[0] == 'I' || word[0] == 'O' || word[0] == 'U')
+            {
+                sb.Append(word + "ma");
+            }
+            else
+            {
+                sb.Append(word.Substring(1, word.Length - 1) + word[0] + "ma");
+            }
+
+            string numberOfA = new String('a', count++);
+            sb.Append(numberOfA + " ");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
