82703afa3f542abb6b85c99010ffdc482203bb64
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-28 15:51:18 +0530

    Day 28

--- Program.cs
+++ Program.cs
@@ -6,12 +6,21 @@ namespace DataStructures
 {
     class Program
     {
+        Dictionary<int, int> values = new Dictionary<int, int>();
+
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            // int k = 0;
+            // foreach (var interval in intervals)
+            // {
+            //     values.Add(interval[0], k++);
+            // }
 
-            ThreeSum threeSum = new ThreeSum();
-            threeSum.Sum(new int[] { -1, 0, 1, 2, -1, 4 });
+
+            // ThreeSum threeSum = new ThreeSum();
+            // threeSum.Sum(new int[] { -1, 0, 1, 2, -1, 4 });
 
             // ReorderList.ListNode head =
             // new ReorderList.ListNode(1,
@@ -40,6 +49,37 @@ namespace DataStructures
             // characters.Query('a');
 
 
+            Console.WriteLine(BinarySearch(2, new int[] { 1, 4, 6, 7, 8, 11, 15, 19, 78 }, 0, 6, 3));
+            //return new int[] { };
+        }
+
+        private static int BinarySearch(int item, int[] newIntervals, int start, int end, int prevMid)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+            int mid = (start + end) / 2;
+            if (newIntervals[mid] == item)
+            {
+                return newIntervals[mid];
+            }
+
+            if (newIntervals[mid] > item)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+
+            if (newIntervals[prevMid] > newIntervals[mid])
+            {
+                return newIntervals[prevMid];
+            }
+
+            return BinarySearch(item, newIntervals, start, end, mid);
         }
     }
 }
