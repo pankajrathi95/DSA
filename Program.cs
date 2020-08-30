18a780c9462627a291ea246e6eddfc82e68d8602
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-08-30 23:13:54 +0530

    Main Program file

--- Program.cs
+++ Program.cs
@@ -9,13 +9,8 @@ namespace DataStructures
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
-            int[][] val = new int[3][];
-            val[0] = new int[] { 3, 4 };
-            val[1] = new int[] { 2, 3 };
-            val[2] = new int[] { 1, 2 };
-
-            FindRightInterval find = new FindRightInterval();
-            find.FindInterval(val);
+            //   2
+            ] xc
         }
     }
 }
