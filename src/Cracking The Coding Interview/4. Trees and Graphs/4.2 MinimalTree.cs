b739ec1217eeab1755c560b44eadddd3271a4764
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-09-08 07:47:34 +0530

    Minimal Tree

---
+++ src/Cracking The Coding Interview/4. Trees and Graphs/4.2 MinimalTree.cs
@@ -0,0 +1,35 @@
+public class MinimalTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode FindMinimalTree(int[] arr)
+    {
+        return CreateMinimalTree(arr, 0, arr.Length - 1);
+    }
+
+    private TreeNode CreateMinimalTree(int[] arr, int start, int end)
+    {
+        if (end < start)
+        {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(arr[mid]);
+        root.left = CreateMinimalTree(arr, start, mid - 1);
+        root.right = CreateMinimalTree(arr, mid + 1, end);
+
+        return root;
+    }
+}
