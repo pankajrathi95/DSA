48ae5394c3750b0d111c436292dc83dbaa5eed07
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-09-17 08:32:09 +0530

    Magic Index

---
+++ src/Cracking The Coding Interview/8. Recursion and Dynamic Programming/8.3 MagicIndex.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class MagicIndex
+{
+    public int FindMagicIndex(int[] arr)
+    {
+        return FindMagicIndex(arr, 0, arr.Length - 1);
+    }
+
+    private int FindMagicIndex(int[] arr, int start, int end)
+    {
+        if (end < start)
+        {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+        if (arr[mid] == mid)
+        {
+            return mid;
+        }
+        else if (arr[mid] > mid)
+        {
+            return FindMagicIndex(arr, start, mid - 1);
+        }
+        else
+        {
+            return FindMagicIndex(arr, mid + 1, end);
+        }
+    }
+}
