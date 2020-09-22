429c194b10693247bfd3cd3dcb1bd864c87fef47
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-09-22 07:56:08 +0530

    Generate Parentheses

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace DataStructures
             var watch = new Stopwatch();
             watch.Start();
             //start here
-
+            Parens parens = new Parens();
+            parens.GenerateParenthesis(3);
             //end here
             watch.Stop();
             Console.WriteLine("Time Taken: " + watch.ElapsedMilliseconds + " ms");
---
+++ src/LeetCode/Medium/GenerateParetheses.cs
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/generate-parentheses
+*/
+
+using System.Collections.Generic;
+
+public class GenerateParetheses
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        IList<string> result = new List<string>();
+        BackTrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void BackTrack(IList<string> result, string sb, int open, int close, int n)
+    {
+        if (open == n && close == n)
+        {
+            result.Add(sb);
+            return;
+        }
+
+        if (open < n)
+        {
+            BackTrack(result, sb + '(', open + 1, close, n);
+        }
+
+        if (close < open)
+        {
+            BackTrack(result, sb + ')', open, close + 1, n);
+        }
+    }
+}
