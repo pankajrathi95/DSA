3836c6d3ff4fcf788935712972fcb1c113ce7fe1
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-09-30 21:55:01 +0530

    leetcode

--- src/LeetCode/30DayLeetCodeChallenge-April2020/6. CheckIfStringValidSequence.cs
+++ src/LeetCode/30DayLeetCodeChallenge-April2020/6. CheckIfStringValidSequence.cs
@@ -1,5 +1,5 @@
 /*
-
+#1430 - https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/
 Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
 
 We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
