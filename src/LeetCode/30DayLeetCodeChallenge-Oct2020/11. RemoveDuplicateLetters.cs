32cdcea26daed29e016b2c1773a096113ce8ed3a
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-10-12 12:19:20 +0530

    Day 11

--- src/LeetCode/30DayLeetCodeChallenge-Oct2020/11. RemoveDuplicateLetters.cs
+++ src/LeetCode/30DayLeetCodeChallenge-Oct2020/11. RemoveDuplicateLetters.cs
@@ -1,5 +1,25 @@
 /*
+#316 - https://leetcode.com/problems/remove-duplicate-letters/
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
 
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+ 
+
+Example 1:
+
+Input: s = "bcabc"
+Output: "abc"
+Example 2:
+
+Input: s = "cbacdcbc"
+Output: "acdb"
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
 */
 
 using System;
