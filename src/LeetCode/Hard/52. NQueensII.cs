ced545e389d9ddb079c6b24745d20d4aef34096b
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-11-25 09:30:08 +0530

    Refactoring

--- src/LeetCode/Hard/52. NQueensII.cs
+++ src/LeetCode/Hard/52. NQueensII.cs
@@ -1,5 +1,27 @@
 /*
 https://leetcode.com/problems/n-queens-ii/
+
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: 2
+Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+Example 2:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 9
 */
 
 using System;
