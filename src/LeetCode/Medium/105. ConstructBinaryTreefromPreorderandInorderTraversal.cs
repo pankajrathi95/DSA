f9e186663f5c37b3e438aeb04e02388dedc2d296
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-11-28 19:00:08 +0530

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/LeetCode/Medium/105. ConstructBinaryTreefromPreorderandInorderTraversal.cs
+++ src/LeetCode/Medium/105. ConstructBinaryTreefromPreorderandInorderTraversal.cs
@@ -1,14 +1,27 @@
 /*
-https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
 */
 
 
 using System.Collections.Generic;
-/**
-* Definition for a binary tree node.
-* 
-*/
-public class BinaryTreeFromPreorderAndInorder
+public class ConstructBinaryTreefromPreorderandInorderTraversal
 {
     public class TreeNode
     {
