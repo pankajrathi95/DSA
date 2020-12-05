6e4307b57afb39e5eebb8a63a5f69841cc91be08
Pankaj Rathi <pankaj_rathi@outlook.com>
2020-12-05 12:37:02 +0530

    Trapping rain water - LeetCode

--- src/LeetCode/Medium/138. CopyListwithRandomPointer.cs
+++ src/LeetCode/Medium/138. CopyListwithRandomPointer.cs
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/problems/copy-list-with-random-pointer
 A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 
 Return a deep copy of the list.
