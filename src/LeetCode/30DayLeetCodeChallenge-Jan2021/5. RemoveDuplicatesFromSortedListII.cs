253a99c83f1a2dfe2cffaf197c60e677dca4642f
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-01-06 13:40:56 +0530

    Day 5 - Jan 2021 - Leetcode

---
+++ src/LeetCode/30DayLeetCodeChallenge-Jan2021/5. RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,54 @@
+/*
+#82 - https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+Example 2:
+
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+
+public class RemoveDuplicatesFromSortedListII {
+    public ListNode DeleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        var prev = fakeHead;
+        var current = head;
+        while (current != null)
+        {
+            while (current.next != null && current.val == current.next.val)
+            {
+                current = current.next;
+            }
+            
+            if (prev.next == current)
+            {
+                prev = prev.next;
+            }
+            else
+            {
+                prev.next = current.next;
+            }
+            
+            current = current.next;
+        }
+        
+        return fakeHead.next;
+    }
+}
