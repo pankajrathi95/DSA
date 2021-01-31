debee3d05691c3dbef32f28b8486c650cbfbf5c5
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-01-31 15:53:47 +0530

    Day 31 - Leetcode

---
+++ src/LeetCode/30DayLeetCodeChallenge-Jan2021/31. NextPermutation.cs
@@ -0,0 +1,78 @@
+/*
+#31 - https://leetcode.com/problems/next-permutation/
+
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
+
+public class NextPermutation
+{
+    public void FindNextPermutation(int[] nums)
+    {
+        int index = nums.Length - 2;
+        while (index >= 0 && nums[index] >= nums[index + 1])
+        {
+            index--;
+        }
+
+        if (index == -1)
+        {
+            Reverse(nums, 0, nums.Length - 1);
+            return;
+        }
+
+        for (int i = nums.Length - 1; i > index; i--)
+        {
+            if (nums[i] > nums[index])
+            {
+                Swap(nums, i, index);
+                break;
+            }
+        }
+
+        Reverse(nums, index + 1, nums.Length - 1);
+    }
+
+    private void Swap(int[] nums, int x, int y)
+    {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+
+    private void Reverse(int[] nums, int start, int end)
+    {
+        while (start < end)
+        {
+            Swap(nums, start++, end--);
+        }
+    }
+}
