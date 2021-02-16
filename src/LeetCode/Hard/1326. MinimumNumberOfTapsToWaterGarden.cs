f79b7871300e37fc3e376016ebcdf24963403214
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-02-16 23:57:20 +0530

    .

--- src/LeetCode/Hard/1326. MinimumNumberOfTapsToWaterGarden.cs
+++ src/LeetCode/Hard/1326. MinimumNumberOfTapsToWaterGarden.cs
@@ -8,7 +8,6 @@ There are n + 1 taps located at points [0, 1, ..., n] in the garden.
 Given an integer n and an integer array ranges of length n + 1 where ranges[i] (0-indexed) means the i-th tap can water the area [i - ranges[i], i + ranges[i]] if it was open.
 
 Return the minimum number of taps that should be open to water the whole garden, If the garden cannot be watered return -1.
-
  
 
 Example 1:
