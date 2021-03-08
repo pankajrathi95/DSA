8c20995a5c8d288b398af1c0c94b7cdc57ce8f70
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-03-08 20:19:40 +0530

    #212

--- src/LeetCode/Hard/212. WordSearchII.cs
+++ src/LeetCode/Hard/212. WordSearchII.cs
@@ -26,63 +26,95 @@ All inputs are consist of lowercase letters a-z.
 The values of words are distinct.
 */
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-
 public class WordSearchII
 {
-    HashSet<string> result = new HashSet<string>();
-    //Timeout exception
-    public IList<string> FindWords(char[][] board, string[] words)
+    public class TrieNode
     {
-        HashSet<string> wordList = new HashSet<string>();
-        int max = 0;
-        foreach (var word in words)
+        public char value;
+        public Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+        public bool isEndOfWord;
+        public TrieNode(char value)
         {
-            max = Math.Max(max, word.Length);
-            wordList.Add(word);
-            Console.WriteLine(word);
+            this.value = value;
+        }
+    }
+
+    public void Insert(string s)
+    {
+        var current = root;
+        foreach (var ch in s)
+        {
+            if (!current.children.ContainsKey(ch))
+            {
+                current.children.Add(ch, new TrieNode(ch));
+            }
+
+            current = current.children[ch];
         }
 
+        current.isEndOfWord = true;
+    }
+
+    private void BuildTrie(string[] words)
+    {
+        foreach (var word in words)
+        {
+            Insert(word);
+        }
+    }
+
+    TrieNode root = new TrieNode(' ');
+    public IList<string> FindWords(char[][] board, string[] words)
+    {
+        BuildTrie(words);
         bool[][] visited = new bool[board.Length][];
         for (int i = 0; i < visited.Length; i++)
         {
             visited[i] = new bool[board[0].Length];
         }
 
+        HashSet<string> result = new HashSet<string>();
         for (int i = 0; i < board.Length; i++)
         {
-            for (int j = 0; j < board.Length; j++)
+            for (int j = 0; j < board[i].Length; j++)
             {
-                FindWords(board, wordList, visited, i, j, "", max);
+                if (root.children.ContainsKey(board[i][j]))
+                {
+                    Dfs(board, visited, i, j, result, root, "");
+                }
             }
         }
 
         return result.ToList();
     }
 
-    private void FindWords(char[][] board, HashSet<string> wordList, bool[][] visited, int i, int j, string str, int max)
+    private void Dfs(char[][] board, bool[][] visited, int i, int j, HashSet<string> result, TrieNode current, string str)
     {
-        if (i < 0 || j < 0 || i >= board.Length || j >= board[i].Length || visited[i][j] || str.Length > max)
+        if (i < 0 || j < 0 || i >= board.Length || j >= board[i].Length || visited[i][j] || !current.children.ContainsKey(board[i][j]))
         {
             return;
         }
 
-
-
         visited[i][j] = true;
-        str = str + board[i][j];
-        if (wordList.Contains(str))
+        current = current.children[board[i][j]];
+        str += board[i][j];
+        if (current.isEndOfWord && !result.Contains(str))
         {
             result.Add(str);
         }
-
-        FindWords(board, wordList, visited, i + 1, j, new String(str), max);
-        FindWords(board, wordList, visited, i, j + 1, new String(str), max);
-        FindWords(board, wordList, visited, i - 1, j, new String(str), max);
-        FindWords(board, wordList, visited, i, j - 1, new String(str), max);
-
+        Dfs(board, visited, i + 1, j, result, current, str);
+        Dfs(board, visited, i, j + 1, result, current, str);
+        Dfs(board, visited, i - 1, j, result, current, str);
+        Dfs(board, visited, i, j - 1, result, current, str);
+        str.Remove(str.Length - 1);
         visited[i][j] = false;
     }
 }
+
+
+
+
+
+
