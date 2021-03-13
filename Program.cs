560e5b46f19e0fb4dc6513338d76f6c1a64e0f3c
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-03-13 17:17:34 +0530

    #day 12 - march 2021 - leetcode

--- Program.cs
+++ Program.cs
@@ -13,15 +13,8 @@ namespace DataStructures
             var watch = new Stopwatch();
             watch.Start();
             //start here
-            IList<string> wordList = new List<string>();
-            wordList.Add("hot");
-            wordList.Add("dog");
-            wordList.Add("dot");
-            wordList.Add("lot");
-            wordList.Add("log");
-            wordList.Add("cog");
-            WordLadder game = new WordLadder();
-            game.LadderLength("hit", "cog", wordList);
+
+
             //end here
             watch.Stop();
             Console.WriteLine("Time Taken: " + watch.ElapsedMilliseconds + " ms");
---
+++ src/LeetCode/30DayLeetCodeChallenge-Mar2021/12. CheckIfaStringContainsAllBinaryCodesofSizeK.cs
@@ -0,0 +1,57 @@
+/*
+#1461 - https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+Given a binary string s and an integer k.
+
+Return True if every binary code of length k is a substring of s. Otherwise, return False.
+
+ 
+
+Example 1:
+
+Input: s = "00110110", k = 2
+Output: true
+Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indicies 0, 1, 3 and 2 respectively.
+Example 2:
+
+Input: s = "00110", k = 2
+Output: true
+Example 3:
+
+Input: s = "0110", k = 1
+Output: true
+Explanation: The binary codes of length 1 are "0" and "1", it is clear that both exist as a substring. 
+Example 4:
+
+Input: s = "0110", k = 2
+Output: false
+Explanation: The binary code "00" is of length 2 and doesn't exist in the array.
+Example 5:
+
+Input: s = "0000000001011100", k = 4
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 10^5
+s consists of 0's and 1's only.
+1 <= k <= 20
+*/
+
+using System;
+using System.Collections.Generic;
+
+public class CheckIfaStringContainsAllBinaryCodesofSizeK
+{
+    public bool HasAllCodes(string s, int k)
+    {
+        HashSet<string> visited = new HashSet<string>();
+        for (int i = 0; i + k <= s.Length; i++)
+        {
+            visited.Add(s.Substring(i, k));
+        }
+
+        return visited.Count == (int)Math.Pow(2, k);
+    }
+}
