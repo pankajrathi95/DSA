926d3cd4ef2401c55ffa467371616d1e8a65da4f
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-06-01 15:00:13 +0530

    Day 1 - June 2021

--- src/LeetCode/Medium/695. MaxAreaOfIsland.cs
+++ src/LeetCode/Medium/695. MaxAreaOfIsland.cs
@@ -1,49 +1,67 @@
 /*
+https://leetcode.com/problems/max-area-of-island/
 
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
 */
 
 using System;
 
 public class MaxAreaOfIsland
 {
-
+    int count = 0;
     public int MaxAreaOfIslands(int[][] grid)
     {
-        int m = grid.Length, n = grid[0].Length;
-        bool[][] visited = new bool[m][];
-        int maxArea = 0;
-
-        for (int i = 0; i < m; i++)
+        int result = 0;
+        for (int i = 0; i < grid.Length; i++)
         {
-            visited[i] = new bool[n];
-        }
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < grid[i].Length; j++)
             {
-                if (!visited[i][j] && grid[i][j] == 1)
+                if (grid[i][j] == 1)
                 {
-                    maxArea = Math.Max(maxArea, Area(i, j, m, n, grid, visited, 1));
+                    count = 0;
+                    Dfs(grid, i, j, grid.Length, grid[i].Length);
+                    result = Math.Max(result, count);
                 }
             }
         }
 
-        return maxArea;
+        return result;
     }
 
-    private int Area(int i, int j, int m, int n, int[][] grid, bool[][] visited, int count)
+    private void Dfs(int[][] grid, int i, int j, int m, int n)
     {
-        if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || grid[i][j] == 0)
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
         {
-            return 0;
+            return;
         }
 
-        visited[i][j] = true;
-        return 1 + Area(i + 1, j, m, n, grid, visited, count + 1) +
-        Area(i - 1, j, m, n, grid, visited, count + 1) +
-        Area(i, j + 1, m, n, grid, visited, count + 1) +
-        Area(i, j - 1, m, n, grid, visited, count + 1);
-
+        grid[i][j] = 0;
+        count++;
+        Dfs(grid, i + 1, j, m, n);
+        Dfs(grid, i, j + 1, m, n);
+        Dfs(grid, i - 1, j, m, n);
+        Dfs(grid, i, j - 1, m, n);
     }
 }
