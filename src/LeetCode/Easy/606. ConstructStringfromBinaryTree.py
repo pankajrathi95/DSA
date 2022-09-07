69d1e8e5028be6b72b4e5ff6d0ebaed6279db7d2
Pankaj Rathi <pankaj_rathi@outlook.com>
2022-09-07 18:21:40 +0530

    Create 606. ConstructStringfromBinaryTree.py

---
+++ src/LeetCode/Easy/606. ConstructStringfromBinaryTree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:        
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ""
+        if root.left is None and root.right is None:
+            return str(root.val)
+        if root.right is None:
+            return str(root.val) + "(" + self.tree2str(root.left) + ")"
+        return str(root.val) + "(" + self.tree2str(root.left) + ")(" + self.tree2str(root.right) + ")"          
+            
